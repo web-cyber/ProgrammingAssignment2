2e2ae19a5e065fc693d3d4aa9653c05f93345deb
web-cyber <alvarez.amy02@gmail.com>
2020-05-17 22:36:28 -0400

    wrote two functions (makeCacheMatrix and cacheSolve) which store the inverses of square matrices in cache to be called from.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These two functions work together to store the value of a square matrix and its inverse and 
+# then call the inverse value when needed.
 
-## Write a short comment describing this function
+# This function returns a list of functions which sets the value of the square matrix and its inverse in a cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inn <- NULL
+        set<- function(y){
+                x<<-y
+                inn<<-NULL
+        }
+        get<- function()x
+        setinn<- function(solve) inn<<- solve
+        getinn<- function()inn
+        list(set=set, get=get,setinn=setinn,
+             getinn=getinn)
 }
 
 
-## Write a short comment describing this function
+# This function takes the functions and values from the makeCacheMatrix function
+# and returns the inverse of that matrix (if possible).
+# If the value is not stored then it will calculate the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inn<-x$getinn()
+        if(!is.null(inn)){
+                message("retrieving from cache")
+                return(inn)
+        } 
+        data<-x$get()
+        inn<-solve(data, ...)
+        x$setinn(inn)
+        inn
 }
